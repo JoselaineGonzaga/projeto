77cabe9b666aad4fe9657066f97f948517f53ddf
Joselaine Duarte Gonzaga <01387674161@seguranca.local>
2024-11-18 10:53:33 -0400

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,17 @@
+/* pegar o html e fazendo troca das classes (toggle(light))*/
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  //pegar a tag img
+  const img = document.querySelector('#profile img')
+
+  //substituie a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    // se tiver sem light mode, manter a imagem nomal
+    img.setAttribute('src', './assets/avatar.png')  
+  }
+}
